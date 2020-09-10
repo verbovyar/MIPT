098746e6e64c074f6374cfc130a8faf130eb076e
Yaroslav <verbovyar@gmail.com>
2020-09-10 14:01:39 +0300

    new struct

--- StructureIntroduction/MyTextSort/MyTextSort/MyTextSort.cpp
+++ StructureIntroduction/MyTextSort/MyTextSort/MyTextSort.cpp
@@ -7,6 +7,42 @@
 const int MAX = 1000;
 const int MAXLEN = 4000;
 
+void writeTxtFile(char* line[], int countOfStr)
+{
+    FILE* fileout = fopen("output.txt", "w");
+
+    for (int k = 0; k < countOfStr; ++k)
+    {
+        fprintf(fileout, "%s\n", line[k]);
+    }
+
+    fclose(fileout);
+}
+
+void sortLines(char* line[], int countOfStr)
+{
+    for (int index1 = 0; index1 < countOfStr; ++index1)
+    {
+        for (int index2 = countOfStr - 1; index2 > index1; index2--)
+        {
+            int i = 0;
+            while (*(line[index2 - 1] + i) == *(line[index2] + i))
+            {
+                ++i;
+            }
+
+            if (*(line[index2 - 1] + i) > * (line[index2] + i))
+            {
+                char* temp = line[index2 - 1];
+                line[index2 - 1] = line[index2];
+                line[index2] = temp;
+            }
+        }
+    }
+
+    writeTxtFile(line, countOfStr);
+}
+
 void separationOnLines(char* line[], char* templine, int countOfStr, int index)
 {
     int j = 0;
@@ -30,40 +66,8 @@ void separationOnLines(char* line[], char* templine, int countOfStr, int index)
 
         line[i] = temp;
     }
-}
 
-void sortLines(char* line[], int countOfStr)
-{
-    for (int index1 = 0; index1 < countOfStr; ++index1)
-    {
-        for (int index2 = countOfStr - 1; index2 > index1; index2--)
-        {
-            int i = 0;
-            while (*(line[index2 - 1] + i) == *(line[index2] + i))
-            {
-                ++i;
-            }
-
-            if (*(line[index2 - 1] + i) > *(line[index2] + i))
-            {
-                char *temp = line[index2 - 1];
-                line[index2 - 1] = line[index2];
-                line[index2] = temp;
-            }
-        }
-    }
-}
-
-void writeTxtFile(char* line[], int countOfStr)
-{
-    FILE* fileout = fopen("output.txt", "w");
-
-    for (int k = 0; k < countOfStr; ++k)
-    {
-        fprintf(fileout, "%s\n", line[k]);
-    }
-
-    fclose(fileout);
+    sortLines(line, countOfStr);
 }
 
 void readTxtFileInOneLine(char* line[])
@@ -94,12 +98,7 @@ void readTxtFileInOneLine(char* line[])
 
     fclose(hamlet);
 
-    // in main;
     separationOnLines(line, templine, countOfStr, index);
-
-    sortLines(line, countOfStr);
-
-    writeTxtFile(line, countOfStr);
 }
 
 int main()
