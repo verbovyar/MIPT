b622c1b827de95a682dd9913ee21ffbf2b60a033
Yaroslav <verbovyar@gmail.com>
2020-10-12 00:37:36 +0300

    new hash

--- StructureIntroduction/myStack/myStack/stackresolution.cpp
+++ StructureIntroduction/myStack/myStack/stackresolution.cpp
@@ -2,36 +2,46 @@
 
 #include "mystack.h"
 
-typedef double elem_t;
+
 
 const elem_t LEFT_CNRY = 0x212121;
 const elem_t RIGHT_CNRY = 0x121212;
 const elem_t POISON_VALUE = 1e+308;
 
-struct myStack {
 
-  #ifdef STACK_DEBUG
+#ifdef STACK_DEBUG
+uint64_t stackHash(myStack* stack) {
+  stack->hash_summ = 0;
   int hash_summ = 0;
-  #endif
-  
-  #ifdef STACK_DEBAG
-  elem_t left_cnry_ = 0;
-  #endif
+  const char* temp = (const char*)stack;
 
-  elem_t* array_ = NULL;
-  int size_ = 0;
-  int capacity_ = 0;
-  int increase_factor_ = 0; 
+  uint64_t size_of_hash = sizeof(myStack);
+  for (int i = 0; i < size_of_hash; ++i) {
+    int bite = temp[i];
+    hash_summ += (bite * i);
+  }
 
-  #ifdef STACK_DEBAG
-  elem_t right_cnry_ = 0;
-  #endif
-};
+  return hash_summ;
+}
+
+bool hashOk(myStack* stack) {
+  uint64_t temp_hash = stack->hash_summ;
+  uint64_t new_hash = stackHash(stack);
+
+  if (temp_hash != new_hash) {
+    return HASH_ERROR;
+  }
+  stack->hash_summ = new_hash;
+
+  return NO_ERROR;
+}
+
+#endif
 
 void construct(myStack* stack, size_t start_size) {
   assert(stack != NULL);
 
-  #ifdef STACK_DEBUG
+#ifdef STACK_DEBUG
   stack->left_cnry_ = LEFT_CNRY;
   stack->right_cnry_ = RIGHT_CNRY;
 
@@ -39,27 +49,31 @@ void construct(myStack* stack, size_t start_size) {
   if (temp == NULL) {
     return;
   }
+  stack->array_ = temp;
   *(elem_t*)stack->array_ = LEFT_CNRY;
-  *(elem_t*)(stack->array_ + sizeof(elem_t) + start_size * sizeof(elem_t)) = RIGHT_CNRY;
-  
-  stack->hash_summ = stackHash(stack);
-    
-  #else
+  *((elem_t*)stack->array_ + 1 + start_size * 1) =
+      RIGHT_CNRY;
+
+#else
 
   elem_t* temp = (elem_t*)calloc(1, start_size * sizeof(elem_t));
   if (temp == NULL) {
     return;
   }
 
-  #endif
+#endif
 
   stack->array_ = temp;
   stack->capacity_ = start_size;
   stack->increase_factor_ = 2;
+  printf("%llu\n", stack->array_[stack->capacity_ + 1]);
 
   for (int i = 1; i <= start_size; ++i) {
     stack->array_[i] = POISON_VALUE;
   }
+  #ifdef STACK_DEBUG
+  stack->hash_summ = stackHash(stack);
+  #endif;
 }
 
 myStack* newStack(size_t start_size) {
@@ -71,78 +85,91 @@ myStack* newStack(size_t start_size) {
 
 void stackNewSize(myStack* stack) {
   assert(stack != NULL);
-    
   ASSERTOK(stack)
 
   if (stack->size_ == stack->capacity_) {
-
-    #ifdef STACK_DEBUG
-    elem_t* temp = (elem_t*)calloc(1, (stack->capacity_ + 2) * stack->increase_factor_ * sizeof(elem_t));  
+#ifdef STACK_DEBUG
+    elem_t* temp = (elem_t*)calloc(
+        1, (stack->capacity_ * stack->increase_factor_ + 2) * sizeof(elem_t));
     if (temp == NULL) {
       return;
     }
 
     free(stack->array_);
+    stack->array_ = temp;
     *(elem_t*)stack->array_ = LEFT_CNRY;
-    *(elem_t*)(stack->array_ + sizeof(elem_t) + (stack->capacity_ * stack->increase_factor_) * sizeof(elem_t)) = RIGHT_CNRY;
+    *((elem_t*)stack->array_ + 
+               (stack->capacity_ * stack->increase_factor_ + 1) * 1) =
+        RIGHT_CNRY;
 
-    stack->hash_summ = stackHash(stack);
-    #else
+#else
 
-    elem_t* temp = (elem_t*)calloc(1, stack->capacity_ * stack->increase_factor_ * sizeof(elem_t));
+    elem_t* temp = (elem_t*)calloc(
+        1, stack->capacity_ * stack->increase_factor_ * 1);
     if (temp == NULL) {
       return;
     }
-    #endif
+#endif
 
-    stack->array_ = temp;
-    stack->capacity_ *= stack->increase_factor_; 
+    //stack->array_ = temp;
+    stack->capacity_ *= stack->increase_factor_;
     for (int i = stack->size_; i < stack->capacity_; ++i) {
       stack->array_[i] = POISON_VALUE;
     }
+    #ifdef STACK_DEBUG
+    stack->hash_summ = stackHash(stack);
+    #endif
   }
 }
 
 void shrinkToFit(myStack* stack) {
   assert(stack != NULL);
 
-  #ifdef STACK_DEBUG
+#ifdef STACK_DEBUG
   if (stack->size_ * stack->increase_factor_ + 1 == stack->capacity_) {
-    elem_t* temp = (elem_t*)calloc(1, (2 + stack->capacity_) / stack->increase_factor_ * sizeof(elem_t));
+    elem_t* temp = (elem_t*)calloc(
+        1, (2 + stack->capacity_) / stack->increase_factor_ * sizeof(elem_t));
     if (temp == NULL) {
       return;
     }
+    stack->array_ = temp;
+    stack->capacity_ /= stack->increase_factor_;
 
     *(elem_t*)stack->array_ = LEFT_CNRY;
-    *(elem_t*)(stack->array_ + sizeof(elem_t) + (stack->capacity_ / stack->increase_factor_) * sizeof(elem_t)) = RIGHT_CNRY;
+    *(elem_t*)(stack->array_ + sizeof(elem_t) +
+               (stack->capacity_ / stack->increase_factor_) * sizeof(elem_t)) =
+        RIGHT_CNRY;
 
     stack->hash_summ = stackHash(stack);
-    #else
-  elem_t* temp = (elem_t*)calloc(1, stack->capacity_ / stack->increase_factor_ * sizeof(elem_t));
+  }
+#else
+  elem_t* temp = (elem_t*)calloc(
+      1, stack->capacity_ / stack->increase_factor_ * sizeof(elem_t));
   if (temp == NULL) {
     return;
   }
-  #endif
-    stack->array_ = temp;
-    stack->capacity_ /= stack->increase_factor_;
+#endif
 }
 
-void push(myStack * stack, int value) {
+void push(myStack* stack, int value) {
   assert(stack != NULL);
-
+  printf("\n%llu\n", stack->hash_summ);
+  printf("yoy %llu\n", stackHash(stack));
+  stack->hash_summ = stackHash(stack);
   ASSERTOK(stack)
+  printf("ZALUPA\n");
 
   stackNewSize(stack);
 
   ++stack->size_;
   stack->array_[stack->size_] = value;
 
-  #ifdef DEBUG
+#ifdef STACK_DEBUG  
   stack->hash_summ = stackHash(stack);
-  #endif
+#endif
 }
 
-void pop(myStack * stack) {
+void pop(myStack* stack) {
   assert(stack != NULL);
 
   ASSERTOK(stack)
@@ -150,9 +177,9 @@ void pop(myStack * stack) {
   stack->array_[stack->size_] = POISON_VALUE;
   --stack->size_;
 
-  #ifdef DEBUG
+#ifdef DEBUG
   stack->hash_summ = stackHash(stack);
-  #endif
+#endif
 }
 
 elem_t top(myStack* stack) {
@@ -164,9 +191,9 @@ elem_t top(myStack* stack) {
 
   ASSERTOK(stack)
 
-  #ifdef DEBUG
+#ifdef DEBUG
   stack->hash_summ = stackHash(stack);
-  #endif
+#endif
 
   return value;
 }
@@ -181,9 +208,9 @@ void deleteStack(myStack* stack) {
   stack->capacity_ = 0;
   free(stack);
 
-  #ifdef DEBUG
+#ifdef DEBUG
   stack->hash_summ = stackHash(stack);
-  #endif
+#endif
 }
 
 void clearStack(myStack* stack) {
@@ -199,64 +226,37 @@ void clearStack(myStack* stack) {
   stack->size_ = 0;
   stack = NULL;
 
-  #ifdef DEBUG
+#ifdef DEBUG
   stack->hash_summ = stackHash(stack);
-  #endif
-}
-
-#ifdef STACK_DEBUG
-  uint64_t stackHash(myStack * stack) {
-  int hash_summ = 0;
-  const char* temp = (const char*)stack;
-
-  uint64_t size_of_hash = sizeof(myStack);
-  for (int i = 0; i < size_of_hash; ++i) {
-    int bite = temp[i]; 
-    hash_summ += (bite * i);
-  }
-
-  return hash_summ;
-}
-
- bool hashOk(myStack* stack) {
-    uint62_t temp_hash = stack->hash_summ;  
-    uint64_t new_hash = stackHash(stack);
-
-    if (temp_hash != new_hash) {
-      return HASH_ERROR;
-    }
-
-    return NO_ERROR;
- }
-
 #endif
+}
 
 STACK_ERROR stackOk(myStack* stack) {
-
   if (stack == NULL) {
     return CONSTRUCT_ERROR;
   }
 
-  #ifdef STACK_DEBUG 
+#ifdef STACK_DEBUG
   if (stack->left_cnry_ != LEFT_CNRY || stack->right_cnry_ != RIGHT_CNRY) {
     return STRUCT_ERROR;
   }
-  #endif
+#endif
 
-  #ifdef STACK_DEBUG 
-  if (HASH_ERROR) {
+#ifdef STACK_DEBUG
+  if (hashOk(stack) != NO_ERROR) {
     return HASH_ERROR;
   }
-  #endif
+#endif
 
-  #ifdef STACK_DEBUG
-  if (stack->array_[0] != LEFT_CNRY || stack->array_[stack->capacity + 1] != RIGHT_CNRY) {
+#ifdef STACK_DEBUG
+  if (stack->array_[0] != LEFT_CNRY ||
+      stack->array_[stack->capacity_ + 1] != RIGHT_CNRY) {
     return ARRAY_ERROR;
   }
 #endif
 
   if (stack->array_ == NULL) {
-    return POINTER_ERROR;    
+    return POINTER_ERROR;
   }
 
   if (stack->size_ == -1) {
@@ -278,9 +278,8 @@ void stackDump(myStack* stack) {
   FILE* log_file = fopen("logfile.txt", "w");
   STACK_ERROR ERROR = stackOk(stack);
   switch (ERROR) {
-
     case (SIZE_ERROR): {
-      fprintf(log_file,"Out of array!\n");
+      fprintf(log_file, "Out of array!\n");
       fprintf(log_file, "Capacity:%d\n", stack->capacity_);
       fprintf(log_file, "Size:%d\n", stack->size_);
 
@@ -313,33 +312,37 @@ void stackDump(myStack* stack) {
       break;
     }
 
-    #ifdef STACK_DEBUG
+#ifdef STACK_DEBUG
     case (HASH_ERROR): {
       fprintf(log_file, "Hash_error, you cant to change buffer!");
+
+      break;
     }
-    #endif
-    
-    #ifdef STACK_DEBUG
+#endif
+
+#ifdef STACK_DEBUG
     case (STRUCT_ERROR): {
       fprintf(log_file, "You cant to change struct(stack)!\n");
-      fprintf(log_file, "Right CNRY:%u \n",stack->right_cnry_);
+      fprintf(log_file, "Right CNRY:%u \n", stack->right_cnry_);
       fprintf(log_file, "Real CNRY:%u", RIGHT_CNRY);
       fprintf(log_file, "Left CNRY:%u \n", stack->left_cnry_);
       fprintf(log_file, "Real CNRY:%u", LEFT_CNRY);
 
       break;
     }
-    #endif
-    
-    #ifdef STACK_DEBUG
+#endif
+
+#ifdef STACK_DEBUG
     case (ARRAY_ERROR): {
       fprintf(log_file, "You cant to change array(stack)!\n");
-      fprintf(log_file, "Right CNRY:%u \n", stack->array_[0]);
-      fprintf(log_file, "Real CNRY:%u", RIGHT_CNRY);
-      fprintf(log_file, "Left CNRY:%u \n", stack->array_[stack->capacity_ + 1]);
-      fprintf(log_file, "Real CNRY:%u", LEFT_CNRY);
+      fprintf(log_file, "Left  CNRY:%llu \n", stack->array_[0]);
+      fprintf(log_file, "Real  CNRY:%llu\n", LEFT_CNRY);
+      fprintf(log_file, "Right CNRY:%llu \n", stack->array_[stack->capacity_ + 1]);
+      fprintf(log_file, "Real  CNRY:%llu \n", RIGHT_CNRY);
+
+      break;
     }
-    #endif
+#endif
   }
 
   fclose(log_file);
